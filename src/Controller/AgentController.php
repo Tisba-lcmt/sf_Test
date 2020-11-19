14f71e2a3f92bd283ff5a024f1f5048db857cc8c
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-11-19 11:14:00 +0100

    Render Twig Path Agents

---
+++ src/Controller/AgentController.php
@@ -0,0 +1,132 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgentController extends AbstractController
+{
+    /**
+     * Je créé la route agent qui sera la nouvelle url de notre site et qui prend en compte le principe
+     * de routing de Symfony avec son nom approprié
+     * J'indique la wildcard en url qui sera remplie par l'id dans mon tableau
+     * multidimensionnel agents
+     * @Route("/agent/{id}", name="agent_show")
+     */
+    // on créé une méthode agentShow qui sera appelée quand l'url "/agent/{id}"
+    // Elle est appelée automatiquement car la route est définie au dessus de la méthode
+
+    // J'indique dans en paramètre de ma méthode $id pour demander à Symfony de mettre la valeur id de mon
+    // tableau en wildcard
+    public function agentShow($id)
+    {
+        // Tableau multidimensionnel $agents
+        $agents = [
+            1 => [
+                "id" => 1,
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "id" => 2,
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "id" => 3,
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "id" => 4,
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "id" => 5,
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => false
+            ]
+        ];
+
+        // J'utilise la méthode render issue de Abstract Controller qui va retourner mon fichier twig
+        // pour le compiler en html afin de le renvoyer en réponse HTTP sur la navigateur
+
+        // En second paramètre du render, je créé un tableau qui contient ma variable twig correspondant
+        // à l'id de mon tableau agents
+        return $this->render('agentwildcard.html.twig', [
+           'agents'=>$agents[$id]
+        ]);
+    }
+
+    /**
+     * Je créé la route agent qui sera la nouvelle url de notre site et qui prend en compte le principe
+     *  de routing de Symfony avec son nom approprié
+     * @Route("/agent", name="agent_list")
+     */
+
+    // on créé une méthode agentShow qui sera appelée quand l'url "/agent/{id}"
+    // Elle est appelée automatiquement car la route est définie au dessus de la méthode
+    public function agentList()
+    {
+        $agents = [
+            1 => [
+                "id" => 1,
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "id" => 2,
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "id" => 3,
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "id" => 4,
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "id" => 5,
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => false
+            ]
+        ];
+
+        // J'utilise la méthode render issue de Abstract Controller qui va retourner mon fichier twig
+        // pour le compiler en html afin de le renvoyer en réponse HTTP sur la navigateur
+
+        // En second paramètre du render, je créé un tableau qui contient ma variable twig correspondant
+        // à l'id de mon tableau agents
+        return $this->render('agent.html.twig', [
+            'agents'=>$agents
+        ]);
+    }
+}
--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -102,47 +102,4 @@ class PagesController extends AbstractController
             'skills' => $skills
         ]);
     }
-
-    /**
-     * @Route("/agent", name="agent")
-     */
-    public function agentShow()
-    {
-        $agents = [
-            1 => [
-                "lastName" => "Robert",
-                "firstName" => "David",
-                "age" => 30,
-                "published" => true
-            ],
-            2 => [
-                "lastName" => "Labaste",
-                "firstName" => "Denis",
-                "age" => 29,
-                "published" => true
-            ],
-            3 => [
-                "lastName" => "Rozand",
-                "firstName" => "Mathieu",
-                "age" => 31,
-                "published" => false
-            ],
-            4 => [
-                "lastName" => "Despert",
-                "firstName" => "Yoann",
-                "age" => 33,
-                "published" => true
-            ],
-            5 => [
-                "lastName" => "Dorignac",
-                "firstName" => "Loic",
-                "age" => 34,
-                "published" => false
-            ]
-        ];
-
-        return $this->render('agent.html.twig', [
-            'agents'=>$agents
-        ]);
-    }
 }
